1d33b7c62bb48a56d73d3456575302b682f0bd5c
lukeify <5379845+lukeify@users.noreply.github.com>
2025-07-13 16:51:21 +1200

    refactor: perform LastRetainedDay calculation in America/New_York

--- utils/retention_period.go
+++ utils/retention_period.go
@@ -4,9 +4,16 @@ import (
 	"time"
 )
 
-func GetLastRetainedDay(now time.Time, n int) time.Time {
+// LastRetainedDay returns the time.Time in UTC that represents the start of the last day in Eastern Time that should
+// have aggregate bars retained for.
+func LastRetainedDay(now time.Time, n int) time.Time {
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		panic(err)
+	}
+
 	i := 0
-	today := now.UTC().Truncate(24 * time.Hour)
+	today := truncateToLocationDay(now.In(loc))
 	curr := today
 
 	for i < n {
@@ -15,5 +22,10 @@ func GetLastRetainedDay(now time.Time, n int) time.Time {
 			i++
 		}
 	}
-	return curr
+
+	return curr.UTC()
+}
+
+func truncateToLocationDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
--- utils/retention_period_test.go
+++ utils/retention_period_test.go
@@ -5,24 +5,24 @@ import (
 	"time"
 )
 
-// TestGetLastRetainedDay_IsAThursdayIfGivenASunday. If the current day is a Sunday, and two business days are retained,
+// TestLastRetainedDay_IsAThursdayIfGivenASunday. If the current day is a Sunday, and two business days are retained,
 // then it should be expected that Thursday and Friday are retained, as Saturday and Sunday are ignored.
-func TestGetLastRetainedDay_IsAThursdayIfGivenASunday(t *testing.T) {
+func TestLastRetainedDay_IsAThursdayIfGivenASunday(t *testing.T) {
 	now := time.Date(2025, 7, 13, 0, 0, 0, 0, time.UTC)
-	expected := time.Date(2025, 7, 10, 0, 0, 0, 0, time.UTC) // Thursday before the weekend
-	result := GetLastRetainedDay(now, 2)
+	expected := time.Date(2025, 7, 10, 4, 0, 0, 0, time.UTC) // Thursday before the weekend, in UTC.
+	result := LastRetainedDay(now, 2)
 
 	if !result.Equal(expected) {
 		t.Errorf("Expected %v but got %v", expected, result)
 	}
 }
 
-// TestGetLastRetainedDay_IsAWednesdayIfGivenAFriday. If the current day is the middle of Friday, and two business days
+// TestLastRetainedDay_IsAWednesdayIfGivenAFriday. If the current day is the middle of Friday, and two business days
 // are retained, then we expect Wednesday and Thursday are trained, as Friday is not complete yet.
-func TestGetLastRetainedDay_IsAWednesdayIfGivenAFriday(t *testing.T) {
+func TestLastRetainedDay_IsAWednesdayIfGivenAFriday(t *testing.T) {
 	now := time.Date(2025, 7, 11, 12, 0, 0, 0, time.UTC)
-	expected := time.Date(2025, 7, 9, 0, 0, 0, 0, time.UTC) // Friday before the weekend
-	result := GetLastRetainedDay(now, 2)
+	expected := time.Date(2025, 7, 9, 4, 0, 0, 0, time.UTC) // Friday before the weekend
+	result := LastRetainedDay(now, 2)
 
 	if !result.Equal(expected) {
 		t.Errorf("Expected %v but got %v", expected, result)
