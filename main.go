14501a70302f866e2e3557219edb0b858af9c84c
lukeify <5379845+lukeify@users.noreply.github.com>
2025-07-16 15:17:12 +1200

    feat: complete top-level backfill functionality

--- main.go
+++ main.go
@@ -5,21 +5,22 @@ import (
 
 	"traderkit-server/database"
 	"traderkit-server/ohlcv"
-	polygonIngestionProvider "traderkit-server/ohlcv/providers"
+	pip "traderkit-server/ohlcv/providers"
 	"traderkit-server/utils"
 )
 
 func main() {
+	// Application startup: load environment variables, initialize a database connection, and backfill any data that
+	// has been missed since last startup.
 	if err := utils.LoadEnvFile(); err != nil {
 		os.Exit(1)
 	}
 	db := database.New()
 
-	// Create an ingestor struct that uses `Polygon` as the ingestion data provider.
-	oi := ohlcv.NewIngestor(db, polygonIngestionProvider.New())
-	// Backfill any unloaded data into the `bars` database table. This may not need to be done if the table is up to
-	// date. Alternatively, it may need to be completely done if the table is empty.
-	oi.Backfill([]string{"AAPL"})
+	// Create an ingestor struct that uses `Polygon` as the ingestion data provider. Then backfill any unloaded data
+	//into the `bars` database table. This may not need to be done if the table is up to date. Alternatively, it may
+	//need to be completely done if the table is empty.
+	ohlcv.NewIngestor(db, pip.New()).Backfill([]string{"AAPL"})
 
 	//if err != nil {
 	//	fmt.Printf("Backfill failed %#v\n", err)
